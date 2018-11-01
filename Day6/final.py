2c767019720a1cb4849a9050c7283deba362773a
praseedm <prasi.praseed@gmail.com>
2018-11-01 20:45:57 +0530

    day 6

--- Day6/final.py
+++ Day6/final.py
@@ -3,6 +3,6 @@ import sys
 fd = open(sys.argv[1],'w')
 i = 0
 for item in [x*int(sys.argv[2]) for x in range(10)]:
-    fd.write(str(i)+" x "+sys.argv[2]+" = "+str(item)+"\n")
+    fd.write("%d x %s = %d\n"%(i,sys.argv[2],item))
     i+=1
 fd.close() 
