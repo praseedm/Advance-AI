9565bfff08d320a80139bb5bc4ed7deac6c85adb
praseedm <prasi.praseed@gmail.com>
2018-11-01 21:27:29 +0530

    day 6

--- Day6/compress.py
+++ Day6/compress.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python
 import sys
-fd = open(sys.argv[1],'w')
-i = 0
-for item in [x*int(sys.argv[2]) for x in range(10)]:
-    fd.write(str(i)+" x "+sys.argv[2]+" = "+str(item)+"\n")
-    i+=1
-fd.close() 
+open(sys.argv[1],'w').write( "\n".join( ["%d x %s = %d"%(x,sys.argv[2],x*int(sys.argv[2])) for x in range(11)] ))
--- Day6/final.py
+++ Day6/final.py
@@ -3,6 +3,6 @@ import sys
 fd = open(sys.argv[1],'w')
 i = 0
 for item in [x*int(sys.argv[2]) for x in range(10)]:
-    fd.write("%d x %s = %d\n"%(i,sys.argv[2],item))
+    fd.write(str(i)+" x "+sys.argv[2]+" = "+str(item)+"\n")
     i+=1
-fd.close() 
+fd.close()
