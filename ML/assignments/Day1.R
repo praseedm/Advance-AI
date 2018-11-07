bb39e22ff4e5b403e3479fcd0871811562b350bc
ai16 <ai16@nielit>
2018-11-07 15:07:57 +0530

    ML day1

--- ML/assignments/Day1.R
+++ ML/assignments/Day1.R
@@ -17,7 +17,7 @@ confusionMatrix(testSet[,5],predictions)
 # Question 3
 ecoliData <- read.table(file.choose())
 data <- ecoliData[,-1]
-dim(data)
+dim(data[,8])
 partition <- createDataPartition(data$V2,p=0.8,list=F)
 dim(partition)
 trainSet <- data[partition,]
@@ -27,5 +27,14 @@ model <- train(V9~.,data=trainSet,method="knn")
 predictions <- predict(model,testSet)
 
 confusionMatrix(testSet[,8],predictions)
-
 # Question 4
+ionData <- read.table(file.choose())
+
+part <- createDataPartition(ionData$V1,p=.9,list=F)
+trainSet = ionData[part,]
+dim(trainSet)
+testSet = ionData[-part,]
+dim(testSet)
+model <- train(V35~.,data=trainSet,method="knn")
+predicts <- predict(model,testSet)
+confusionMatrix(testSet[,35],predicts)
