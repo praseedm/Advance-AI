48329504ca56dafd26de0e7614b2d5e99b91bcb1
ai16 <ai16@nielit>
2018-11-07 14:52:04 +0530

    ML day1

---
+++ ML/assignments/Day1.R
@@ -0,0 +1,31 @@
+library(caret)
+# Question 1
+?createDataPartition
+partition <-  createDataPartition(iris$Species,p=0.75,list=FALSE)
+trainSet <- iris[partition,]
+dim(trainSet)
+testSet <- iris[-partition,]
+dim(testSet)
+model <- train(Species~.,data=trainSet,method="knn")
+
+predictions <- predict(model,testSet)
+
+# Question 2
+confusionMatrix(testSet[,5],predictions)
+?caretFuncs
+
+# Question 3
+ecoliData <- read.table(file.choose())
+data <- ecoliData[,-1]
+dim(data)
+partition <- createDataPartition(data$V2,p=0.8,list=F)
+dim(partition)
+trainSet <- data[partition,]
+testSet <- data[-partition,]
+dim(testSet)
+model <- train(V9~.,data=trainSet,method="knn")
+predictions <- predict(model,testSet)
+
+confusionMatrix(testSet[,8],predictions)
+
+# Question 4
